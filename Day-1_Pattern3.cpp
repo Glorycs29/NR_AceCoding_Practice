2fde30471f350076ebb648a0d409ca55874605a6
glory1607 <77358738+Glorycs29@users.noreply.github.com>
2023-06-08 11:23:26 +0530

    Add files via upload

---
+++ Day-1_Pattern3.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n; cin>>n;
+    
+    // n-i spaces and then i star
+    for(int i=0;i<n;i++){
+        for(int j=n-i;j>0;j--){
+            cout<<" ";
+        }
+        for(int j=0;j<2*i+1;j++){
+            cout<<"*";
+        }
+        cout<<"\n";
+    }
+}
