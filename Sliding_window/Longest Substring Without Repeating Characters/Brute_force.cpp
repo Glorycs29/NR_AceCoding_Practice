a0cbebda6ac79e77790e5ea64a7b38f65f004a3d
Glory Gupta <prikarshglory@gmail.com>
2024-02-11 23:41:14 +0530

    Update Brute_force.cpp

--- Sliding_window/Longest Substring Without Repeating Characters/Brute_force.cpp
+++ Sliding_window/Longest Substring Without Repeating Characters/Brute_force.cpp
@@ -5,7 +5,7 @@ public:
         int n = s.length();
         int res = 0;
         for(int i=0; i<n; i++){
-            for(int j=i; j,n; j++){
+            for(int j=i; j<n; j++){
                 if(uniqueAll(s,i,j)){
                     res = max(res, j-i+1);
                 }
