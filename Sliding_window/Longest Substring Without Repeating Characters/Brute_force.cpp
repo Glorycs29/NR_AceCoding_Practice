ccdf8a662b29ba57d615d64c9e81a44e148572d5
Glory Gupta <prikarshglory@gmail.com>
2024-02-11 23:28:14 +0530

    Update Brute_force.cpp

--- Sliding_window/Longest Substring Without Repeating Characters/Brute_force.cpp
+++ Sliding_window/Longest Substring Without Repeating Characters/Brute_force.cpp
@@ -1,1 +1,31 @@
 
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int n = s.length();
+        int res = 0;
+        for(int i=0; i<n; i++){
+            for(int j=i; j,n; j++){
+                if(uniqueAll(s,i,j)){
+                    res = max(res, j-i+1);
+                }
+            }
+        }
+        return res;
+    }
+
+    bool uniqueAll(string &s, int start, int end){
+        unordered_set<char> chars;
+
+        for(int i=start; i<=end; i++){
+            char c = s[i];
+            if(chars.count(c)){
+                return false;
+            }
+            chars.insert(c);
+        }
+
+        return true;
+    }
+
+};
